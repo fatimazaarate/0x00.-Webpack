1536fe10bc4988dd7214b2e35e3bb0bad8c251da
fatimazaarate <fatima.zaarate@gmail.com>
2024-07-08 21:29:13 +0100

    task_0

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,43 +1,50 @@
-// CourseList/CourseListRow.js
-
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-  if (isHeader) {
+
+const rowStyle = {
+  backgroundColor: '#f5f5f5ab',
+};
+const headerRowStyle = {
+  backgroundColor: '#deb5b545',
+};
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+  let tr = undefined;
+
+  if (isHeader === true) {
     if (textSecondCell === null) {
-      return (
-        <tr>
-          <th colSpan="2">{textFirstCell}</th>
-        </tr>
-      );
+      tr = <th colSpan='2' style={headerRowStyle}>{textFirstCell}</th>;
     } else {
-      return (
-        <tr>
-          <th>{textFirstCell}</th>
-          <th>{textSecondCell}</th>
-        </tr>
+      tr = (
+        <Fragment>
+          <th style={headerRowStyle}>{textFirstCell}</th>
+          <th style={headerRowStyle}>{textSecondCell}</th>
+        </Fragment>
       );
     }
-  } else {
-    return (
-      <tr>
-        <td>{textFirstCell}</td>
-        <td>{textSecondCell}</td>
-      </tr>
+  }
+  if (isHeader === false) {
+    tr = (
+      <Fragment>
+        <td style={rowStyle}>{textFirstCell}</td>
+        <td style={rowStyle}>{textSecondCell}</td>
+      </Fragment>
     );
   }
-}
 
-CourseListRow.propTypes = {
-  isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.string,
+  return <tr>{tr}</tr>;
 };
 
 CourseListRow.defaultProps = {
   isHeader: false,
   textSecondCell: null,
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default CourseListRow;
